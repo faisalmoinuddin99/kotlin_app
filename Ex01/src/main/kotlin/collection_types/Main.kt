0723f92bb4a620884eaf6eb5f400acc84922430b
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-08-25 10:55:36 +0530

    Acronym Problem Solved using collection transformation on String

--- Ex01/src/main/kotlin/collection_types/Main.kt
+++ Ex01/src/main/kotlin/collection_types/Main.kt
@@ -1,5 +1,28 @@
 package org.example.collection_types
 
+import kotlin.reflect.typeOf
+
 fun main() {
     println("welcome to CollectionTypes in Kotlin")
+
+    val text: String ="Something - I made up from thin air"
+    val result = generate({ pharse ->
+        pharse.split(" ")
+            .asSequence()
+            .flatMap { it.split("_") }
+            .flatMap { it.split("-") }
+            .filter { it.isNotEmpty() }
+            .map { it.first().uppercase() }
+            .joinToString("")
+    }, text)
+
+    println(result) // SIMUFTA
+
+
 }
+
+fun generate(operation: (String) -> String, phrase: String): String {
+
+    return operation(phrase)
+}
+
